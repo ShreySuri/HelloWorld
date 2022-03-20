4acb359ffb3fe32f7db32aea0864f7faa74e05e3
Shrey Suri <shreysuri5@gmail.com>
2022-03-20 18:02:38 -0500

    Changed how text is displayed

---
+++ Helloworld2.py
@@ -0,0 +1,17 @@
+def helloworld_ribbon ():
+    print("H")
+    print("  e")
+    print("    l")
+    print("      l")
+    print("        o")
+    print("")
+    print("        W")
+    print("      o")
+    print("    r")
+    print("  l")
+    print("d")
+    print("")
+    
+for i in range (1,10):
+    helloworld_ribbon()
+    
