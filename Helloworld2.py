fbc34ea7d765a3064fd17c08b3862dace6753a1a
Shrey Suri <shreysuri5@gmail.com>
2022-06-19 18:55:31 -0500

    adding changes

--- Helloworld2.py
+++ Helloworld2.py
@@ -12,9 +12,20 @@ def helloworld_ribbon_vertical():
     print("d")
     print("")
 
-def helloworld_ribbon_horizontal():
-    string_1 = "H            d    "
-    string_2 = " e          l     "
-    print("  l        r      ")
-    print("   l      o       ")
-    print("    o    W        ")
+string_1 = "H            d    "
+string_2 = " e          l     "
+string_3 = "  l        r      "
+string_4 = "   l      o       "
+string_5 = "    o    W        "
+
+
+text_format = None
+while text_format != "vertical" or text_format != "horizontal":
+    print("How would you like this to be formatted? ")
+    text_format = input(print("Type 'horizontal' or 'vertical'. ")
+    print("")
+
+repeat = 0.5
+while repeat % 1 != 0:
+    print("How many times would you like this to be repeated? ")
+    repeat = input(print("Please enter an integer. ")
